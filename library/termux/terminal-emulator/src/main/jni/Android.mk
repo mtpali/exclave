765d3d67de9ea45f77225eabf7e822bb5fdf819a
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2024-12-17 00:14:56 +0800

    build: strip embeded paths and ndk build-id

--- library/termux/terminal-emulator/src/main/jni/Android.mk
+++ library/termux/terminal-emulator/src/main/jni/Android.mk
@@ -2,5 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE:= libtermux
 LOCAL_SRC_FILES:= termux.cpp
+LOCAL_LDFLAGS := -Wl,--build-id=none
+LOCAL_CFLAGS := -ffile-prefix-map=${ANDROID_HOME}/ndk=ndk -ffile-prefix-map=${PWD}=Exclave
 include $(BUILD_SHARED_LIBRARY)
 APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
