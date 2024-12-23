247d546b374b8e69374170a3215156209c7a19cd
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2024-12-23 23:39:55 +0800

    Update gradle to 8.12 and update other dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     implementation(project(":library:termux:terminal-view"))
     implementation(project(":library:termux:terminal-emulator"))
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-ktx:1.9.3")
     implementation("androidx.fragment:fragment-ktx:1.8.5")
@@ -88,6 +88,6 @@ dependencies {
 
     implementation(project(":library:proto-stub"))
 
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.3")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
