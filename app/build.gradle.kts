56814c4b8f564000a1545199f81e7c5104af3e0e
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-04-25 15:41:39 +0800

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.8.9")
     implementation("androidx.navigation:navigation-ui-ktx:2.8.9")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.work:work-runtime-ktx:2.10.0")
-    implementation("androidx.work:work-multiprocess:2.10.0")
+    implementation("androidx.work:work-runtime-ktx:2.10.1")
+    implementation("androidx.work:work-multiprocess:2.10.1")
 
     implementation("com.takisoft.preferencex:preferencex:1.1.0")
     implementation("com.takisoft.preferencex:preferencex-simplemenu:1.1.0")
@@ -60,7 +60,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("cn.hutool:hutool-core:5.8.37")
     implementation("cn.hutool:hutool-json:5.8.37")
-    implementation("com.google.code.gson:gson:2.13.0")
+    implementation("com.google.code.gson:gson:2.13.1")
     implementation("com.google.zxing:core:3.5.3")
 
     implementation("org.yaml:snakeyaml:2.4")
@@ -76,9 +76,9 @@ dependencies {
         exclude(group = "androidx.appcompat")
     }
 
-    implementation("androidx.room:room-runtime:2.7.0")
-    ksp("androidx.room:room-compiler:2.7.0")
-    implementation("androidx.room:room-ktx:2.7.0")
+    implementation("androidx.room:room-runtime:2.7.1")
+    ksp("androidx.room:room-compiler:2.7.1")
+    implementation("androidx.room:room-ktx:2.7.1")
 
     implementation("com.blacksquircle.ui:editorkit:2.0.0")
     implementation("com.blacksquircle.ui:language-json:2.0.0")
