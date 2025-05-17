c09ceb9081342a97c20da1abc020daa9724ecd27
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-05-17 16:47:39 +0800

    Update dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,9 +7,9 @@ apply(from = "../repositories.gradle.kts")
 
 dependencies {
     implementation("com.android.tools.build:gradle:8.9.2")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
-    implementation("cn.hutool:hutool-http:5.8.37")
-    implementation("cn.hutool:hutool-crypto:5.8.37")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.21")
+    implementation("cn.hutool:hutool-http:5.8.38")
+    implementation("cn.hutool:hutool-crypto:5.8.38")
     implementation("org.kohsuke:github-api:1.327")
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.14")
     implementation("com.google.protobuf:protobuf-gradle-plugin:0.9.5")
