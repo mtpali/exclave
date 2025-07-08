59871dd284c16f7ee4bd8af51a1ecaccc0fb3964
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-07-08 22:19:26 +0800

    Fix "ignore routing rules with all packages uninstalled"

--- app/src/main/java/io/nekohasekai/sagernet/fmt/ConfigBuilder.kt
+++ app/src/main/java/io/nekohasekai/sagernet/fmt/ConfigBuilder.kt
@@ -1584,7 +1584,7 @@ fun buildV2RayConfig(
                     continue
                 }
                 for (pkg in rule.packages) {
-                    PackageCache[pkg]?.takeIf { it >= 10000 }?.let {
+                    PackageCache[pkg]?.takeIf { it >= 1000 }?.let {
                         uidList.add(it)
                     }
                 }
