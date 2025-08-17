833d15eeacfc2454f359914c285d5d140768b2eb
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-08-17 15:25:51 +0800

    Temporarily revert "Update gradle to 9.0.0 and AGP to 8.12.0" for F-Droid

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 apply(from = "../repositories.gradle.kts")
 
 dependencies {
-    implementation("com.android.tools.build:gradle:8.12.0")
+    implementation("com.android.tools.build:gradle:8.11.1")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.0")
     implementation("cn.hutool:hutool-http:5.8.39")
     implementation("cn.hutool:hutool-crypto:5.8.39")
     implementation("org.kohsuke:github-api:1.329")
-    implementation("com.squareup.okhttp3:okhttp:5.1.0")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.16")
     implementation("com.google.protobuf:protobuf-gradle-plugin:0.9.5")
 }
