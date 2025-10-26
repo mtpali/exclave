f5f79d5a25e1038487e61a1a0f16d4dc15d3822c
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-10-26 23:55:18 +0800

    Update dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ apply(from = "../repositories.gradle.kts")
 
 dependencies {
     implementation("com.android.tools.build:gradle:8.11.1")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.20")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.21")
     implementation("org.kohsuke:github-api:1.330")
     implementation("com.squareup.okhttp3:okhttp:5.2.1")
     implementation("com.google.protobuf:protobuf-gradle-plugin:0.9.5")
--- library/core/go.mod
+++ library/core/go.mod
@@ -91,4 +91,4 @@ replace gvisor.dev/gvisor => gvisor.dev/gvisor v0.0.0-20250429202743-3a608a52255
 
 //replace github.com/v2fly/v2ray-core/v5 => ../../../v2ray-core
 
-replace github.com/v2fly/v2ray-core/v5 => github.com/dyhkwong/v2ray-core/v5 v5.41.1-0.20251024134928-9d3232c92f29
+replace github.com/v2fly/v2ray-core/v5 => github.com/dyhkwong/v2ray-core/v5 v5.41.1-0.20251026101046-92daf01e41a4
