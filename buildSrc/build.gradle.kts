3352a1108e8a3cd4a75bc4c8f6b919f48642575c
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-12-15 21:43:47 +0800

    Update dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 apply(from = "../repositories.gradle.kts")
 
 dependencies {
-    implementation("com.android.tools.build:gradle:8.13.1")
+    implementation("com.android.tools.build:gradle:8.13.2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.21")
     implementation("org.kohsuke:github-api:1.330")
     implementation("com.squareup.okhttp3:okhttp:5.3.2")
