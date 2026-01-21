ea6bb777e3ea2447041ea4bfbc33676805e95c32
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2026-01-21 22:03:13 +0800

    Remove useless

--- plugin/api/build.gradle.kts
+++ plugin/api/build.gradle.kts
@@ -1,15 +1,9 @@
 plugins {
     id("com.android.library")
-    id("kotlin-parcelize")
 }
 
 setupCommon()
 
 android {
     namespace = "io.nekohasekai.sagernet.plugin"
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-        }
-    }
 }
