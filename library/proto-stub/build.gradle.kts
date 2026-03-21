ef2f226189dd12b5faac3d5b97ff59f83a8e8dcb
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2026-03-21 15:45:08 +0800

    Update dependencies

--- library/proto-stub/build.gradle.kts
+++ library/proto-stub/build.gradle.kts
@@ -23,7 +23,7 @@ android {
 
 protobuf {
     protoc {
-        artifact = "com.google.protobuf:protoc:4.34.0"
+        artifact = "com.google.protobuf:protoc:4.34.1"
     }
     generateProtoTasks {
         all().forEach {
