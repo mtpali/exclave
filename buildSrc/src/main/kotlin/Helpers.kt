cf08ade6def06ce9422e61efb172ec9e74608e83
MTPALI <alimtp@atomicmail.io>
2026-08-23 14:08:04 +0330

    build: filter universal APK native libraries to ARM only

--- buildSrc/src/main/kotlin/Helpers.kt
+++ buildSrc/src/main/kotlin/Helpers.kt
@@ -116,6 +116,7 @@ fun Project.setupAppCommon(projectName: String = "") {
         }
 
         defaultConfig.targetSdk = 37
+        defaultConfig.ndk.abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a"))
         buildTypes.getByName("release") {
             @Suppress("UnstableApiUsage")
             vcsInfo.include = false
