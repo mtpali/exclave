d1e1df8841d89c4481bda5c58b19359e5ba4ee7f
MTPALI <alimtp@atomicmail.io>
2026-08-24 16:24:02 +0330

    fix ping refresh adapter lookup

--- app/src/main/java/io/nekohasekai/sagernet/ui/ConfigurationFragment.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/ConfigurationFragment.kt
@@ -1717,7 +1717,7 @@ class ConfigurationFragment @JvmOverloads constructor(
                                 ProfileManager.updateProfile(proxyEntity)
                                 onMainDispatcher {
                                     bindingAdapterPosition.takeIf { it != RecyclerView.NO_POSITION }
-                                        ?.let(this@ConfigurationAdapter::notifyItemChanged)
+                                        ?.let { configurationListView.adapter?.notifyItemChanged(it) }
                                 }
                             }
                         }
